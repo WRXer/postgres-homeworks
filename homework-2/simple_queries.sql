6651353a45e7692a5b7a5dcc6a601380196344ec
WRXer <king_311@mail.ru>
2023-04-20 21:30:53 +0400

    Исправил номер 2 в 1 ч дз

--- homework-2/simple_queries.sql
+++ homework-2/simple_queries.sql
@@ -3,7 +3,7 @@
 SELECT DISTINCT contact_name, country FROM customers;
 
 -- 2. идентификатор заказа и разницу между датами формирования (order_date) заказа и его отгрузкой (shipped_date) из таблицы orders
-SELECT DISTINCT order_id, order_date, shipped_date FROM orders;
+SELECT order_id, AGE(shipped_date, order_date) FROM orders;
 
 -- 3. все города без повторов, в которых зарегистрированы заказчики (customers)
 SELECT DISTINCT city FROM customers;
