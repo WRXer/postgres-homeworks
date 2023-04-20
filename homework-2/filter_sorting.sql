683ed697601b240bf45541c198c51b13185d4363
WRXer <king_311@mail.ru>
2023-04-20 21:23:08 +0400

    Исправил номер 3

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -8,7 +8,7 @@ SELECT DISTINCT ship_country, ship_city FROM orders
 ORDER BY ship_country, ship_city
 
 -- 3. сколько дней в среднем уходит на доставку товара в Германию (таблица orders, колонки order_date, shipped_date, ship_country)
-SELECT AVG(AGE(order_date, shipped_date)) FROM orders
+SELECT AVG(AGE(shipped_date,order_date)) FROM orders
 WHERE ship_country IN ('Germany');
 
 -- 4. минимальную и максимальную цену среди продуктов, не снятых с продажи (таблица products, колонки unit_price, discontinued не равно 1)
